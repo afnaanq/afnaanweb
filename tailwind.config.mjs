c7d0d8ee01e5da5fbd9fddf8fac6b21673b2dd46
afnaan qasim <afnaan.qasim2@gmail.com>
2025-12-01 18:12:19 +0000

    logo added and dark mode tweaked

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,44 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: 'var(--gray-0)',
+          50: 'var(--gray-50)',
+          100: 'var(--gray-100)',
+          200: 'var(--gray-200)',
+          300: 'var(--gray-300)',
+          400: 'var(--gray-400)',
+          500: 'var(--gray-500)',
+          600: 'var(--gray-600)',
+          700: 'var(--gray-700)',
+          800: 'var(--gray-800)',
+          900: 'var(--gray-900)',
+          999: 'var(--gray-999)',
+        },
+        accent: {
+          light: 'var(--accent-light)',
+          DEFAULT: 'var(--accent-regular)',
+          dark: 'var(--accent-dark)',
+        },
+      },
+      fontFamily: {
+        body: 'var(--font-body)',
+        brand: 'var(--font-brand)',
+      },
+      fontSize: {
+        'sm': 'var(--text-sm)',
+        'base': 'var(--text-base)',
+        'md': 'var(--text-md)',
+        'lg': 'var(--text-lg)',
+        'xl': 'var(--text-xl)',
+        '2xl': 'var(--text-2xl)',
+        '3xl': 'var(--text-3xl)',
+        '4xl': 'var(--text-4xl)',
+        '5xl': 'var(--text-5xl)',
+      },
+    },
   },
   plugins: [],
 }
