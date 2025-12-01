eb77ba4b4fa32fa5578f0479f21eb9782a4c1fb1
afnaan qasim <afnaan.qasim2@gmail.com>
2025-12-01 21:01:57 +0000

    styling of indexpage

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 
 export default function Greeting({messages}) {
-
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
+  
   const [greeting, setGreeting] = useState(messages[0]);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const changeGreeting = () => {
+    setIsVisible(false);
+    
+
+    setTimeout(() => {
+      setGreeting(randomMessage());
+      setIsVisible(true);
+    }, 500);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(changeGreeting, 10000);
+
+    return () => clearInterval(interval);
+  }, [messages]);
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
-      </button>
+      <h1 
+        class={`transition-all duration-500 ease-in-out ${
+          isVisible 
+            ? 'opacity-100 translate-y-0' 
+            : 'opacity-0 -translate-y-2'
+        }`}
+      >
+        {greeting}, my name is Afnaan Qasim.
+      </h1>
     </div>
   );
 }
