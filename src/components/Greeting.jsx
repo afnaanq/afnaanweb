f5f39eb77d37a26d66780340b10dc00158f98d11
afnaan qasim <afnaan.qasim2@gmail.com>
2025-12-15 14:53:29 +0000

    switched logo - skill pill - edits to nav bar

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -31,8 +31,7 @@ export default function Greeting({messages}) {
             : 'opacity-0 -translate-y-2'
         }`}
       >
-        {greeting}, my name is<br/>
-        Afnaan Qasim.
+        {greeting}, my name is Afnaan Qasim.
       </h1>
     </div>
   );
